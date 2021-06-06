a222ee1681c4b6413da944187cb63c2d5e244e0c
elicitmind <elicitmind@gmail.com>
2021-06-06 09:56:11 +0200

    connect html file and server ready to run

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express")
 
 const app = express()
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({
+    extended: true
+}))
 
 
 
-app.get("/", (req, res) => res.send("hello")
-)
+app.get("/", (req, res) => {
+
+    let today = new Date()
+    let currentDay = today.getDay()
+
+    if (currentDay === 6 || currentDay === 0) {
+        res.sendFile(__dirname + "/index.html")
+    } else {
+        res.send("<h1>Witam Cię.</h2>")
+    }
+})
 
 
 app.listen(3000, () => console.log("server UP on port 3000"))
