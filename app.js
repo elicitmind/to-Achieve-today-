617fda26f74d0deb1cbd61c539d5cee8dbe24312
elicitmind <elicitmind@gmail.com>
2021-06-07 10:10:06 +0200

    add esj and to currentDay responses, html and style

--- app.js
+++ app.js
@@ -5,18 +5,52 @@ app.use(express.urlencoded({
     extended: true
 }))
 
+app.set("view engine", "ejs")
 
+//maandag	Monday
+// dinsdag	Tuesday
+// woensdag	Wednesday
+// donderdag	Thursday
+// vrijdag	Friday
+// zaterdag	Saturday
+// zondag
 
 app.get("/", (req, res) => {
 
     let today = new Date()
     let currentDay = today.getDay()
+    let day = ""
 
-    if (currentDay === 6 || currentDay === 0) {
-        res.sendFile(__dirname + "/index.html")
-    } else {
-        res.send("<h1>Witam Cię.</h2>")
+    switch (currentDay) {
+        case 0:
+            day = "ZONDAG"
+            break;
+        case 1:
+            day = "MAANDAG"
+            break;
+        case 2:
+            day = "DINSDAG"
+            break;
+        case 3:
+            day = "WOENSDAG"
+            break;
+        case 4:
+            day = "DONDERDAG"
+            break;
+        case 5:
+            day = "VRIJDAG"
+            break;
+        case 6:
+            day = "ZATERDAG"
+            break;
+        default:
+            alert("SPACE AND TIME. LOG = " + currentDay)
+            break;
     }
+
+    res.render("list", {
+        kindOfDay: day
+    })
 })
 
 
